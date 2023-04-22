bb17be0eab7f99038bb611108a1584b3b0e3ec13
Kianush Atighi-Moghadam Solís <k.atighimoghadda01@ufromail.cl>
2023-04-22 19:26:38 -0400

    Develop visualizador_restriccion

--- visualizador_restriccion/src/root.component.js
+++ visualizador_restriccion/src/root.component.js
@@ -1,3 +1,16 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+
+  const ulStyle = {color : 'white', fontSize:'23px', width: '50%'}
+  const titleStyle = {color : 'white', fontSize:'46px'}
+  const greenColorStyle = {color : '#2eae00'}
+
+  return (
+    <div>
+    <h1 style={titleStyle}>Visualizador de restricción de uso de leña en Temuco</h1>
+    <img src="https://www.nuevoaire.cl/wp-content/uploads/2020/05/poligoos-copia.jpg" alt="Mapa de restricciones de uso de leña" />
+    <p style={ulStyle}>
+    El uso de leña puede estar restringido en ciertas áreas debido a preocupaciones ambientales o de salud pública. Asegúrese de revisar las regulaciones locales antes de utilizar leña.
+      </p>
+  </div>
+  );
 }
